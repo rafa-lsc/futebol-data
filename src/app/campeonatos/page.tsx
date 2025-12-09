2b6826e3cc20f8295065a6970e758e267bb2dece
Eduardo Brandao <eduardobrandao0311@gmail.com>
2025-12-09 17:05:18 -0300

    feat: estrutura da parte de campeonatos

--- src/app/campeonatos/page.tsx
+++ src/app/campeonatos/page.tsx
@@ -1,6 +1,82 @@
+"use client";
+
+import Header from "@/src/components/Header/Header";
+import { useState } from "react";
+
+const API_TOKEN = "live_1464f8a638cb9f7b0b2b4d3fdd2e9e";
 
 export default function Campeonatos() {
-    return(
-        <h1>Campeonatos</h1>
-    );
+  const [artilharia, setArtilharia] = useState(""); //armazenar os dados dos artilheiros
+  const [loading, setLoading] = useState(false); //saber se requisicao esta em andamento
+  const [error, setError] = useState(""); //saber se requisicao falhou
+  const [secaoAtiva, setSecaoAtiva] = useState(""); //determinar qual esta ativo
+
+  const handleFetchArtilharia = async () => {
+    setLoading(true);
+
+    const urlArt =
+      "https://api.api-futebol.com.br/v1/campeonatos/20/artilharia";
+    try {
+      const res = await fetch(urlArt, {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${API_TOKEN}`,
+        },
+      });
+      const dados = await res.json();
+      setArtilharia(dados);
+      setSecaoAtiva("artilharia");
+      console.log(artilharia);
+    } catch (err) {
+      setError("Falha ao carregar a artilharia");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div className="min-h-screen bg-gray-900">
+      <Header />
+      <main className="flex flex-col items-center py-2 flex-grow gap-4">
+        <h1 className="text-5xl text-white font-bold">
+          Informações - Champions League
+        </h1>
+        <div className="flex gap-2">
+          <button
+            onClick={handleFetchArtilharia}
+            className="text-xl text-white cursor-pointer bg-blue-500 rounded-lg py-1 px-2"
+          >
+            🦶 Artilharia
+          </button>
+
+          <button className="text-xl text-white cursor-pointer bg-blue-500 rounded-lg py-1 px-2">
+            ⚽ Partidas
+          </button>
+        </div>
+        {/* exibir os resultados */}
+        {/* carregando */}
+        {loading && <p className="text-white">Carregando...</p>}
+        {/* TODO: erro */}
+        {/* resultado */}
+        {secaoAtiva === "artilharia" &&
+          artilharia !== "" &&
+          Array.isArray(artilharia) && (
+            <div>
+              <div className="grid grid-cols-3 text-xl text-blue-500">
+                <p>Nome</p>
+                <p>Gols</p>
+                <p>Time</p>
+              </div>
+              {artilharia.map((jogador) => (
+                <div className="grid grid-cols-3 bg-gray-700 text-white border border-gray-900">
+                  <p>{jogador.atleta.nome_popular}</p>
+                  <p>{jogador.gols}</p>
+                  <p>{jogador.time.nome_popular}</p>
+                </div>
+              ))}
+            </div>
+          )}
+      </main>
+    </div>
+  );
 }
---
+++ src/components/Camp/index.tsx
@@ -0,0 +1,7 @@
+export default function Camp(){
+  return (
+    <div>
+      
+    </div>
+  )
+}
