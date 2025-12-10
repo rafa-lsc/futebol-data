d56516c6f4096775c6f17a2e971eaa7ba9111234
Eduardo Brandao <eduardobrandao0311@gmail.com>
2025-12-09 23:52:47 -0300

    feat: permitindo adicionar palpite

--- src/app/palpites/page.tsx
+++ src/app/palpites/page.tsx
@@ -1,12 +1,112 @@
+"use client";
+
 import HeaderLogado from "@/src/components/HeaderLogado";
 import Jogo from "@/src/components/Jogo";
+import { useState } from "react";
 
 export default function Palpites() {
+  const [mostraForm, setMostraForm] = useState(false);
+  const [formInfo, setFormInfo] = useState({nomeCamp:'', nomeTimeA: '', nomeTimeB: '', qtdGolsA: 0, qtdGolsB: 0})
+  const [adicionaPalpite, setAdicionaPalpite] = useState(false)
+  const [erro, setErro] = useState("");
+
+  const handleInserir = () => {
+    setMostraForm(!mostraForm);
+    setAdicionaPalpite(false)
+    const apagaPalpite = {
+      nomeCamp: "",
+      nomeTimeA: "",
+      nomeTimeB: "",
+      qtdGolsA: 0,
+      qtdGolsB: 0,
+    }
+    setFormInfo(apagaPalpite)
+  };
+
+  const handleAdicionaPalpite = (e) => {
+    e.preventDefault();
+
+    const form = e.target;
+
+    if(form.nomeCamp.value === "" || form.nomeTimeA.value === "" || form.nomeTimeB.value === "" || Number(form.qtdGolsA.value) < 0 || Number(form.qtdGolsB.value) < 0){
+      setAdicionaPalpite(false)
+      alert("Verifique se todos os campos foram preenchidos e se a quantidade de gols é maior que 0");
+    }else{
+      const novoPalpite = {
+        nomeCamp: form.nomeCamp.value,
+        nomeTimeA: form.nomeTimeA.value,
+        nomeTimeB: form.nomeTimeB.value,
+        qtdGolsA: Number(form.qtdGolsA.value),
+        qtdGolsB: Number(form.qtdGolsB.value),
+      }
+      setFormInfo(novoPalpite);
+      setAdicionaPalpite(true);
+      setMostraForm(!mostraForm)
+    }
+  }
+
   return (
     <div className="min-h-screen bg-gray-900">
       <HeaderLogado />
       <main className="flex-grow">
-        <Jogo />
+        <div className="flex items-center justify-center py-4">
+          <button
+            onClick={handleInserir}
+            className="bg-green-700 cursor-pointer rounded-lg py-2 px-1 text-white hover:bg-green-500 transition-colors"
+          >
+            🆕 Adicionar Palpite
+          </button>
+        </div>
+
+        {/* mostrar ou não formulário para adicionar palpite */}
+        {mostraForm && (
+          <div className="flex justify-center rounded-lg">
+            <form
+              onSubmit={handleAdicionaPalpite}
+              className="bg-gray-700 text-white flex flex-col gap-3 justify-center items-center px-3 py-2 rounded-lg"
+              action=""
+            >
+              <div className="flex gap-3">
+                <label htmlFor="nomeCamp">Nome do Campeonato: </label>
+                <input
+                  className="bg-white"
+                  type="text"
+                  id="nomeCamp"
+                  name="nomeCamp"
+                  placeholder="ex.: Champions"
+                />
+              </div>
+              <div className="flex gap-3">
+                <label htmlFor="nomeTimeA">Nome do Primeiro Time: </label>
+                <input type="text" id="nomeTimeA" name="nomeTimeA" />
+              </div>
+              <div className="flex gap-3">
+                <label htmlFor="qtdGolsA">
+                  Quantidade de gols do Primeiro Time:{" "}
+                </label>
+                <input type="number" id="qtdGolsA" name="qtdGolsA" />
+              </div>
+              <div className="flex gap-3">
+                <label htmlFor="nomeTimeB">Nome do Segundo Time: </label>
+                <input type="text" id="nomeTimeB" name="nomeTimeB" />
+              </div>
+              <div className="flex gap-3">
+                <label htmlFor="qtdGolsB">
+                  Quantidade de gols do Segundo Time:{" "}
+                </label>
+                <input type="number" id="qtdGolsB" name="qtdGolsB" />
+              </div>
+              <button type="submit" className="bg-blue-500 cursor-pointer p-2 rounded-lg hover:bg-blue-700 transition-colors text-white">
+                Adicionar!
+              </button>
+            </form>
+          </div>
+        )}
+
+        {adicionaPalpite && (
+          <Jogo nomeCampeonato={formInfo.nomeCamp} timeA={formInfo.nomeTimeA} timeB={formInfo.nomeTimeB} golsA={formInfo.qtdGolsA} golsB={formInfo.qtdGolsB}/>
+        )}
+
       </main>
     </div>
   );
--- src/components/Jogo/index.tsx
+++ src/components/Jogo/index.tsx
@@ -1,10 +1,18 @@
-export default function Jogo() {
+interface JogoProps{
+  nomeCampeonato: string,
+  timeA: string,
+  timeB: string,
+  golsA: number,
+  golsB: number
+}
+
+export default function Jogo({nomeCampeonato, timeA, timeB, golsA, golsB}: JogoProps) {
   return (
     <div className="bg-gray-600 flex flex-col gap-5 rounded-lg w-1/3 py-2 m-5 justify-center items-center">
       {/* nome do campeonato */}
       <div className="w-1/1 border-b border-gray-900 py-2">
         <h2 className="text-xl text-white font-bold text-center">
-          🏆 Campeonato Brasileiro Série A
+          🏆 {nomeCampeonato}
         </h2>
       </div>
 
@@ -13,14 +21,14 @@ export default function Jogo() {
         <div className="flex gap-5 justify-center py-5 items-center">
           {/* time A */}
           <div className="flex gap-4 justify-center items-center">
-            <p className="text-2xl">CORINTHIANS</p>
-            <p className="text-4xl font-bold"> 2 </p>
+            <p className="text-2xl">{timeA}</p>
+            <p className="text-4xl font-bold"> {golsA} </p>
           </div>
           <p className="text-2xl"> X </p>
           {/* time B */}
           <div className="flex gap-4 justify-center items-center">
-            <p className="text-4xl font-bold"> 1 </p>
-            <p className="text-2xl">CRUZEIRO</p>
+            <p className="text-4xl font-bold"> {golsB} </p>
+            <p className="text-2xl">{timeB}</p>
           </div>
         </div>
 
